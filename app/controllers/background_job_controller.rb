2c42fce0bc11d9ec07866ceb3bb90570deec391f
Juan Ruiz <invitado@perdidita.(none)>
2014-04-08 18:28:58 +0100

    Finished local resque

---
+++ app/controllers/background_job_controller.rb
@@ -0,0 +1,11 @@
+class BackgroundJobController < ApplicationController
+  def index
+    (1..10).each do |i|
+      puts "Adding to queue job #{1}"
+      Resque.enqueue(ResqueJob, i)
+
+      #simulate some time between requests
+      sleep 1.5
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ResqueApp::Application.config.secret_key_base = '4199c6ee75ff0d9db2766677b4d62221fd69465ae5bcb8f49a9260d3f176da8cd03c23c33878fb0c511c5baabf953132c3a0c8a23d77473468f15060f2e1058d'
